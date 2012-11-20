e08301e2f4faa66d80c54b5bfe26cb9c97880f3f
tr <tr@>
2012-11-20 19:00:12 +0000

    minor change to js

--- Resources/public/js/polycollections.js
+++ Resources/public/js/polycollections.js
@@ -3,12 +3,12 @@ $(function(){
 
     $('li.polycollection-types').click(function(){
         // pull data from elements
-        var operatorType = $(this).data('type');
+        var type = $(this).data('type');
         var itemPrototype = $(this).data('prototype');
         var liPrototype = $('ul.polycollection').data('prototype');
 
         // replace with a default title and insert li prototype into ul li wrapper prototype
-        itemPrototype = itemPrototype.replace(/__name__label__/g, operatorType + ' ' + polyCollectionCounter++);
+        itemPrototype = itemPrototype.replace(/__name__label__/g, type + ' ' + polyCollectionCounter++);
         var combinedPrototype = liPrototype.replace(/__ITEM__/, itemPrototype);
 
         // add to the list of polycollection
